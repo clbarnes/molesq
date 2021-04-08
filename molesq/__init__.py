9ae9a75eefd1ab3199293a389d5d2fa6dd748fa3
Chris Barnes <barnesc@janelia.hhmi.org>
2021-04-08 16:36:05 +0100

    remove references to strategy

--- molesq/__init__.py
+++ molesq/__init__.py
@@ -1,6 +1,6 @@
 from .version import version as __version__  # noqa: F401
 from .version import version_tuple as __version_info__  # noqa: F401
 
-from .transform import Strategy, Transformer
+from .transform import Transformer
 
-__all__ = ["Strategy", "Transformer"]
+__all__ = ["Transformer"]
